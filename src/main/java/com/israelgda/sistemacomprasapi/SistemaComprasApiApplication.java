903cea48811e96ad2e3df4de079bb29b5ff2f667
Israel Gadelha <gdelha.israel@gmail.com>
2021-12-19 14:01:41 -0300

    Feat: Commit inicial do projeto

---
+++ src/main/java/com/israelgda/sistemacomprasapi/SistemaComprasApiApplication.java
@@ -0,0 +1,13 @@
+package com.israelgda.sistemacomprasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaComprasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaComprasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/israelgda/sistemacomprasapi/SistemaComprasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israelgda.sistemacomprasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaComprasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
